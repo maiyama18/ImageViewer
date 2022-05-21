8b7c2a73aedd7bb656a022c09e734dd72b3c73a4
maiyama <myam0118@gmail.com>
2022-05-21 12:11:04 +0900

    Add extension to use ImageViewer to show network image

--- Examples/ImageViewerSwiftUIExample/ImageViewerSwiftUIExample/ContentView.swift
+++ Examples/ImageViewerSwiftUIExample/ImageViewerSwiftUIExample/ContentView.swift
@@ -2,15 +2,29 @@ import SwiftUI
 import ImageViewer
 
 struct ContentView: View {
-    @State private var isPresented: Bool = false
+    @State private var isImagePresented: Bool = false
+    @State private var isAsyncImagePresented: Bool = false
     
     var body: some View {
-        image
+        VStack {
+            image
+                .frame(width: 200, height: 200)
+                .onTapGesture {
+                    isImagePresented = true
+                }
+            
+            AsyncImage(
+                url: URL(string: "https://picsum.photos/id/870/300/300"),
+                content: { $0.resizable() },
+                placeholder: { Color.gray.opacity(0.25) }
+            )
             .frame(width: 200, height: 200)
             .onTapGesture {
-                isPresented = true
+                isAsyncImagePresented = true
             }
-            .imageViewer(isPresented: $isPresented, image: image)
+        }
+        .imageViewer(isPresented: $isImagePresented, image: image)
+        .imageViewer(isPresented: $isAsyncImagePresented, url: URL(string: "https://picsum.photos/id/870/300/300"))
     }
     
     private var image: some View {
--- Sources/ImageViewer/View+Extension.swift
+++ Sources/ImageViewer/View+Extension.swift
@@ -4,4 +4,10 @@ extension View {
     public func imageViewer<I: View>(isPresented: Binding<Bool>, image: I) -> some View {
         modifier(ImageViewerModifier(isPresented: isPresented, image: image))
     }
+    
+    @available(iOS 15.0, *)
+    public func imageViewer(isPresented: Binding<Bool>, url: URL?) -> some View {
+        let image = AsyncImage(url: url, content: { $0.resizable() }, placeholder: { Color.clear })
+        return modifier(ImageViewerModifier(isPresented: isPresented, image: image))
+    }
 }
