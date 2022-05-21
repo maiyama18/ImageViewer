4e2b8d58048f71a15a60f8d1ab05a8a34b8a7663
maiyama <myam0118@gmail.com>
2022-05-21 11:00:54 +0900

    Use ImageViewer from example project

--- Examples/ImageViewerSwiftUIExample/ImageViewerSwiftUIExample/ContentView.swift
+++ Examples/ImageViewerSwiftUIExample/ImageViewerSwiftUIExample/ContentView.swift
@@ -2,8 +2,22 @@ import SwiftUI
 import ImageViewer
 
 struct ContentView: View {
+    @State private var isPresented: Bool = false
+    
     var body: some View {
-        Text("Hello, world!")
+        image
+            .frame(width: 200, height: 200)
+            .onTapGesture {
+                isPresented = true
+            }
+            .imageViewer(isPresented: $isPresented, image: image)
+    }
+    
+    private var image: some View {
+        Image(systemName: "sun.min")
+            .resizable()
+            .foregroundColor(.white)
             .padding()
+            .background(Color.orange)
     }
 }
