1bd1f0b53067aa218bef25e44aa7c5079302d6af
maiyama <myam0118@gmail.com>
2022-05-21 12:39:37 +0900

    Improve example

--- Examples/ImageViewerSwiftUIExample/ImageViewerSwiftUIExample/ContentView.swift
+++ Examples/ImageViewerSwiftUIExample/ImageViewerSwiftUIExample/ContentView.swift
@@ -6,21 +6,31 @@ struct ContentView: View {
     @State private var isAsyncImagePresented: Bool = false
     
     var body: some View {
-        VStack {
-            image
-                .frame(width: 200, height: 200)
-                .onTapGesture {
-                    isImagePresented = true
+        ScrollView {
+            VStack(spacing: 24) {
+                VStack(spacing: 4) {
+                    Text("system image")
+                        .bold()
+                    image
+                        .frame(width: 200, height: 200)
+                        .onTapGesture {
+                            isImagePresented = true
+                        }
                 }
-            
-            AsyncImage(
-                url: URL(string: "https://picsum.photos/id/870/300/300"),
-                content: { $0.resizable() },
-                placeholder: { Color.gray.opacity(0.25) }
-            )
-            .frame(width: 200, height: 200)
-            .onTapGesture {
-                isAsyncImagePresented = true
+                
+                VStack(spacing: 4) {
+                    Text("network image")
+                        .bold()
+                    AsyncImage(
+                        url: URL(string: "https://picsum.photos/id/870/300/300"),
+                        content: { $0.resizable() },
+                        placeholder: { Color.gray.opacity(0.25) }
+                    )
+                    .frame(width: 200, height: 200)
+                    .onTapGesture {
+                        isAsyncImagePresented = true
+                    }
+                }
             }
         }
         .imageViewer(isPresented: $isImagePresented, image: image)
