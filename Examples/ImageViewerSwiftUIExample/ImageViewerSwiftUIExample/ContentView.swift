e752774d6255cee71887cd21fd74fbf0c0bd3148
maiyama <myam0118@gmail.com>
2022-05-22 11:01:04 +0900

    Improve SwiftUI example layout

--- Examples/ImageViewerSwiftUIExample/ImageViewerSwiftUIExample/ContentView.swift
+++ Examples/ImageViewerSwiftUIExample/ImageViewerSwiftUIExample/ContentView.swift
@@ -6,35 +6,43 @@ struct ContentView: View {
     @State private var isAsyncImagePresented: Bool = false
     
     var body: some View {
-        ScrollView {
-            VStack(spacing: 24) {
-                VStack(spacing: 4) {
-                    Text("system image")
-                        .bold()
-                    image
-                        .frame(width: 200, height: 200)
-                        .onTapGesture {
-                            isImagePresented = true
-                        }
-                }
-                
-                VStack(spacing: 4) {
-                    Text("network image")
-                        .bold()
-                    AsyncImage(
-                        url: URL(string: "https://picsum.photos/id/870/300/300"),
-                        content: { $0.resizable() },
-                        placeholder: { Color.gray.opacity(0.25) }
-                    )
+        VStack(spacing: 24) {
+            VStack(spacing: 4) {
+                Text("system image")
+                    .bold()
+                image
                     .frame(width: 200, height: 200)
                     .onTapGesture {
-                        isAsyncImagePresented = true
+                        isImagePresented = true
                     }
+            }
+            
+            VStack(spacing: 4) {
+                Text("network image")
+                    .bold()
+                AsyncImage(
+                    url: URL(string: "https://picsum.photos/id/870/300/300"),
+                    content: { $0.resizable() },
+                    placeholder: { Color.gray.opacity(0.25) }
+                )
+                .frame(width: 200, height: 200)
+                .onTapGesture {
+                    isAsyncImagePresented = true
                 }
             }
         }
         .imageViewer(isPresented: $isImagePresented, image: image)
-        .imageViewer(isPresented: $isAsyncImagePresented, url: URL(string: "https://picsum.photos/id/870/300/300"), placeholder: { Color.gray.opacity(0.25) })
+        .imageViewer(
+            isPresented: $isAsyncImagePresented,
+            url: URL(string: "https://picsum.photos/id/870/300/300"),
+            placeholder: {
+                Color.black
+                    .overlay(
+                        ProgressView().scaleEffect(5)
+                            .foregroundColor(.white)
+                    )
+            }
+        )
     }
     
     private var image: some View {
