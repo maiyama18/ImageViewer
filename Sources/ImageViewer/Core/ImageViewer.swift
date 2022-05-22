dd81a2f4da5aed17443693226e7e0c3e1359f313
maiyama <myam0118@gmail.com>
2022-05-22 21:53:16 +0900

    Add share button

--- Sources/ImageViewer/Core/ImageViewer.swift
+++ Sources/ImageViewer/Core/ImageViewer.swift
@@ -1,8 +1,13 @@
 import SwiftUI
 
+extension UIImage: Identifiable {}
+
 struct ImageViewer: View {
     @Binding var isPresented: Bool
     var dataSource: ImageDataSource
+    
+    @State private var isShareViewPresented: Bool = false
+    @State private var shareItems: [Any] = []
     
     var body: some View {
         Group {
@@ -29,9 +34,35 @@ struct ImageViewer: View {
                         }
                         
                         Spacer()
+                            
+                        Button(action: {
+                            switch dataSource {
+                            case .url(let url):
+                                guard let url = url else { return }
+                                URLSession.shared.dataTask(with: url) { data, _, error in
+                                    guard let data = data, let uiImage = UIImage(data: data) else {
+                                        print(error ?? "unknown error")
+                                        return
+                                    }
+                                    shareItems = [uiImage]
+                                    isShareViewPresented = true
+                                }.resume()
+                            case .uiImage(let uIImage):
+                                shareItems = [uIImage]
+                                isShareViewPresented = true
+                            }
+                        }) {
+                            Image(systemName: "square.and.arrow.up")
+                                .font(.title2)
+                                .foregroundColor(.white)
+                                .padding()
+                        }
                     }
                 }
             }
+        }
+        .sheet(isPresented: $isShareViewPresented) {
+            ShareView(items: shareItems)
         }
     }
 }
---
+++ Sources/ImageViewer/Core/ShareView.swift
@@ -0,0 +1,12 @@
+import UIKit
+import SwiftUI
+
+struct ShareView: UIViewControllerRepresentable {
+    var items: [Any]
+    
+    func makeUIViewController(context: Context) -> UIActivityViewController {
+        UIActivityViewController(activityItems: items, applicationActivities: nil)
+    }
+    
+    func updateUIViewController(_ uiViewController: UIActivityViewController, context: Context) {}
+}
